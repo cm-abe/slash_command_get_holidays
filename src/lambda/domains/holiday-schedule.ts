669176dc319165e3da8ac54ca923a020ced54051
abe.shinsuke <abe.shinsuke@classmethod.jp>
2019-02-10 19:45:37 +0900

    コマンド文字列がセットされていなかった場合のエラーを実装

--- src/lambda/domains/holiday-schedule.ts
+++ src/lambda/domains/holiday-schedule.ts
@@ -4,7 +4,9 @@ import { ApplicationError } from '../exceptions/ApplicationError';
 export class HolidaySchedule {
     constructor(commandParameter: SlashCommandParameter | null) {
         if(commandParameter != null) {
-            
+            if(commandParameter.command == null) {
+                throw new ApplicationError("コマンドがセットされていません。");
+            }
         } else {
             throw new ApplicationError("コマンドパラメータがありません。");
         }
--- test/lambda/domains/holiday-schecule.test.ts
+++ test/lambda/domains/holiday-schecule.test.ts
@@ -3,9 +3,9 @@ import * as assert from 'power-assert';
 import 'mocha';
 import { ApplicationError } from '../../../src/lambda/exceptions/ApplicationError';
 import { HolidaySchedule } from '../../../src/lambda/domains/holiday-schedule';
+import { SlashCommandParameter } from '../../../src/lambda/domains/slash-command-parameter';
 
 // 「いつ」「誰が」休みをとるのか
-// TODO 引数にインプットパラメータを渡さなかった場合はエラーになる
 // TODO インプットパラメータのコマンド名がget_holidaysではなかった場合はエラーにする
 // TODO インプットパラメータのテキストが日付形式ではなかった場合はエラーにする
 // TODO インプットパラメータのテキストは日付形式を複数受けることができる
@@ -17,6 +17,32 @@ describe("休暇スケジュール", () => {
             },
             (error: ApplicationError) => {
                 assert(error.message === "コマンドパラメータがありません。");
+                return true;
+            }
+        )
+    });
+
+    it("引数のcommand文字列がセットされていなかった場合はエラーになる", () => {
+        assert.throws(
+            () => {
+                new HolidaySchedule(new class implements SlashCommandParameter{
+                    token: string;
+                    teamId: string;
+                    teamDomain: string;
+                    enterpriseId: string;
+                    enterpriseName: string;
+                    channelId: string;
+                    channelName: string;
+                    userId: string;
+                    userName: string;
+                    command: string;
+                    text: string;
+                    responseUrl: string;
+                    triggerId: string;
+                });
+            },
+            (error: ApplicationError) => {
+                assert(error.message == "コマンドがセットされていません。");
                 return true;
             }
         )
