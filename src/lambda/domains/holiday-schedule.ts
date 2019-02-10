f137bc950d89b1523735f8a7f984fe76a338991d
abe.shinsuke <abe.shinsuke@classmethod.jp>
2019-02-10 20:06:03 +0900

    コマンド名のバリデーションを関数に変更

--- src/lambda/domains/holiday-schedule.ts
+++ src/lambda/domains/holiday-schedule.ts
@@ -4,14 +4,20 @@ import { ApplicationError } from '../exceptions/ApplicationError';
 export class HolidaySchedule {
     constructor(commandParameter: SlashCommandParameter | null) {
         if(commandParameter != null) {
-            if(commandParameter.command == null) {
-                throw new ApplicationError("コマンドがセットされていません。");
-            }
-            if(commandParameter.command != "get-holidays") {
-                throw new ApplicationError("コマンド名が違います。 " + commandParameter.command);
-            }
+            validateCommand(commandParameter);
         } else {
             throw new ApplicationError("コマンドパラメータがありません。");
         }
+    }
+}
+
+function validateCommand(commandParameter: SlashCommandParameter) {
+    const commandName = "get-holidays";
+
+    if(commandParameter.command == null) {
+        throw new ApplicationError("コマンドがセットされていません。");
+    }
+    if(commandParameter.command != commandName) {
+        throw new ApplicationError("コマンド名が違います。 " + commandParameter.command);
     }
 }
