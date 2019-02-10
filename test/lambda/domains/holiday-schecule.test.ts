d0a767fbdff8b7664bb149533b948907654f6a37
abe.shinsuke <abe.shinsuke@classmethod.jp>
2019-02-10 15:11:00 +0900

    パラメータのnullエラーテスト(Red)

--- test/lambda/domains/holiday-schecule.test.ts
+++ test/lambda/domains/holiday-schecule.test.ts
@@ -1,9 +1,22 @@
 // test/lamdba/domains/holiday-schecule.test.ts
 import * as assert from 'power-assert';
-
+import {describe, it} from 'mocha';
 
 // 「いつ」「誰が」休みをとるのか
 // TODO 引数にインプットパラメータを渡さなかった場合はエラーになる
 // TODO インプットパラメータのコマンド名がget_holidaysではなかった場合はエラーにする
 // TODO インプットパラメータのテキストが日付形式ではなかった場合はエラーにする
 // TODO インプットパラメータのテキストは日付形式を複数受けることができる
+describe("休暇スケジュール", () => {
+    it("引数にインプットパラメータを渡さなかった場合はエラーになる", () => {
+        assert.throws(
+            () => {
+                new HolidaySchedule(null);
+            },
+            (error) => {
+                assert(error.message === "");
+                return true;
+            }
+        )
+    });
+});
