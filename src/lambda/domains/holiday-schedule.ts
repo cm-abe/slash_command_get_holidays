263c4c5d0ce99e9e1be5a2e156d87e70f24f9728
abe.shinsuke <abe.shinsuke@classmethod.jp>
2019-02-17 17:15:38 +0900

    lintメッセージの対応(残

--- src/lambda/domains/holiday-schedule.ts
+++ src/lambda/domains/holiday-schedule.ts
@@ -1,30 +1,30 @@
-import { ISlashCommandParameter } from './slash-command-parameter';
-import { ApplicationError } from '../exceptions/ApplicationError';
-import * as moment from 'moment';
-import { GoogleCalenderRequest as Calendar } from '../infrastructures/google-calendar-request';
+import * as moment from "moment";
+import { ApplicationError } from "../exceptions/ApplicationError";
+import { GoogleCalenderRequest as Calendar } from "../infrastructures/google-calendar-request";
+import { ISlashCommandParameter } from "./slash-command-parameter";
 
 export class HolidaySchedule {
     public userName: string;
-    public holidayList: Array<string> = [];
-    public parseErrorList: Array<string> = [];
-    public insertSuccessList: Array<string> = [];
-    public insertErrorList: Array<string> = [];
+    public holidayList: string[] = [];
+    public parseErrorList: string[] = [];
+    public insertSuccessList: string[] = [];
+    public insertErrorList: string[] = [];
 
     constructor(commandParameter: ISlashCommandParameter | null) {
-        if(commandParameter != null) {
+        if (commandParameter != null) {
             validateCommand(commandParameter);
 
-            if(commandParameter.text == null) {
+            if (commandParameter.text == null) {
                 throw new ApplicationError("日付の指定がありません。");
             }
 
             commandParameter.text.split(" ").forEach((dateString) => {
-                if(moment(dateString, "YYYY-MM-DD").isValid()) {
+                if (moment(dateString, "YYYY-MM-DD").isValid()) {
                     this.holidayList.push(dateString);
                 } else {
                     this.parseErrorList.push(dateString);
                 }
-            })
+            });
 
             this.userName = commandParameter.userName;
         } else {
@@ -33,24 +33,24 @@ export class HolidaySchedule {
     }
 
     // カレンダーへのリクエスト
-    insertHolidays() {
+    public insertHolidays() {
         this.holidayList.forEach((dateString) => {
             Calendar.insertHolidayEvent(Calendar.generateEvent(this.userName, dateString)).then((eventHtml) => {
                 this.insertSuccessList.push(eventHtml);
             }).catch(() => {
                 this.insertErrorList.push(dateString);
             });
-        })
+        });
     }
 }
 
 function validateCommand(commandParameter: ISlashCommandParameter) {
     const commandName = "get-holidays";
 
-    if(commandParameter.command == null) {
+    if (commandParameter.command == null) {
         throw new ApplicationError("コマンドがセットされていません。");
     }
-    if(commandParameter.command != commandName) {
+    if (commandParameter.command !== commandName) {
         throw new ApplicationError("コマンド名が違います。 " + commandParameter.command);
     }
 }
